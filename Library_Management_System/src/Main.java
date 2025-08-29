1e48d93eac50e27a9c3723904338df3b6af79690
Yellareddy07 <131944447+Yellareddy07@users.noreply.github.com>
2025-08-29 20:44:05 +0530

    Update Main.java

--- Library_Management_System/src/Main.java
+++ Library_Management_System/src/Main.java
@@ -4,8 +4,6 @@ import LIBRARY.Library;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
         Library lib=new Library();
